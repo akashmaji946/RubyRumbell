0c3630bd7ab16cd852782b2cae054be50271cc49
Akash Maji <akashmaji946@gmail.com>
2023-04-13 16:44:55 +0530

    more on arrays

--- arrays_more.rb
+++ arrays_more.rb
@@ -221,6 +221,243 @@ as, bs = strs.partition{ |str| str.include?("k") }
 p as, bs
 
 
+# split() method can aceept any string, not just one length string
+puts
+arr = "This is a long string, with many spaces in between"
+p arr.split
+p arr.split(" ")
+p arr.split(",")
+p arr.split("a")
+
+puts
+arr.split.each do |item| puts item.length end
 
 
+# WAP to find longest word in a sentence
+puts
+sentence = "This is a long set of strings, with many spaces in between"
+def find_longest(sentence)
 
+    maxl = 0
+    maxw = ""
+
+    sentence.split.each do |word|
+        if word.length > maxl
+            maxl = word.length
+            maxw = word 
+        end
+    
+    end
+
+    [maxl, maxw]
+
+end
+
+maxl, maxw = find_longest(sentence)
+p maxl,maxw
+
+# split and chars on string object
+puts
+dialog = "Hey, I am happy"
+p dialog.chars
+p dialog.split("")
+
+# iterate over a string
+dialog.each_char { |ch| puts ch}
+puts
+
+# join() method
+puts
+sentence = "This is a long set of strings, with many spaces in between"
+
+sent_arr = sentence.split(" ")
+new_sent = sent_arr.join("$")
+puts sentence
+puts new_sent
+# default is empty char
+new_sent = sent_arr.join
+puts new_sent
+
+
+# WAP to customly join an array of items by a delimeter
+puts
+sentence = "A B C"
+# way1
+def myjoin1(items, delim = "") 
+    str = ""
+    items.each { |word|
+         str << word << delim
+    }
+    str[0, str.length-delim.length]
+end
+# way2
+def myjoin2(items, delim = "") 
+    str = ""
+    last_index = items.length - 1
+    items.each_with_index { |word, index|
+         str << word 
+         str << delim if index < last_index 
+    }
+    str
+end
+puts myjoin1(sentence.split)
+puts myjoin1(sentence.split, "%$%")
+puts myjoin2(sentence.split)
+puts myjoin2(sentence.split, "%$%")
+
+
+# count() method on a ruby string
+# counts the total sum of occurences of each distinct char in the param
+puts
+str = "Aww..this is a very long string in my idea. Wow amazingly good"
+puts str.count(".")
+puts str.count("a")
+puts str.count("Aa")
+puts str.count("Ww")
+puts str.count("Aw")
+# same as above (only distinct chars in the string in any order matters)
+puts str.count("Aww")
+
+# WAP to implement custom count()
+puts
+str = "Aww..this is a very long string in my idea. Wow amazingly good"
+def mycount(str, param = "")
+    counter = 0
+    str.each_char { |ch|
+        counter += 1 if param.include?(ch)
+    }
+    counter
+end
+puts mycount(str)
+puts
+puts mycount(str, ".")
+puts mycount(str, "a")
+puts mycount(str, "Aa")
+puts mycount(str, "Ww")
+puts mycount(str, "Aw")
+# same as above (only distinct chars in the string in any order matters)
+puts mycount(str, "Aww")
+
+# index and rindex on strings
+puts
+str = "this is a string in ruby"
+puts str.index("i")
+puts str.rindex("i")
+
+# search index from  particular position
+puts str.index("i", 3)
+puts str.rindex("i", 16)
+puts
+# we can index a substring also
+puts str.index("is", 3)
+
+
+# WAP for custom index()
+str = "this is a string in ruby"
+def myindex(str, substr= " ", start = 0)
+    len_substr = substr.length
+    start.upto(str.length - substr.length){ |index|
+         seq = str[index, len_substr]
+        #  puts seq, substr
+         return index if seq == substr
+    }
+    return nil 
+end
+puts
+p myindex(str, "is", 3)
+
+puts
+typo = "Georg Washinton"
+puts typo
+typo.insert(5, "e")
+puts typo
+#typo = "Georg Washinton"
+typo.insert(-1, "e!")
+puts typo
+
+# squeeze() method in ruby
+# removes all duplicates coming in a consecutive way
+puts
+sentence = "thiss iis a sttttring in   rrrrubyyyy" 
+# squeeze doest modify the object
+puts sentence.squeeze
+puts sentence
+sentence.squeeze!
+puts sentence
+
+# we can specify (in a substring) what characters to be removed in duplicacy
+puts
+sentence = "thiss iis a sttttring in   rrrrubyyyy" 
+puts sentence.squeeze!("rit")
+puts sentence
+
+#  WAP to define own custom squeeze method
+puts
+sentence = "thiss iis a sttttring in   rrrrubyyyy" 
+def mysqueeze(str)
+   final_str = ""
+   str.chars.each_with_index do |v, i|
+        if v == str[i+1]
+            next
+        else 
+            final_str << v
+        end
+   end
+   return final_str
+end
+puts mysqueeze(sentence)
+
+def mysqueeze2(str, params)
+    final_str = ""
+    str.chars.each_with_index do |v, i|
+         if v == str[i+1] && params.include?(v)
+             next
+         else 
+             final_str << v
+         end
+    end
+    return final_str
+end
+puts
+puts mysqueeze2(sentence, " ")
+puts mysqueeze2(sentence, "srity ")
+
+sent = "ttttttttttt hhhhhhhhh iiiiiii sssssss aaaa     thisa"
+puts mysqueeze2(sent, "thisa ")
+
+
+# delete() method in ruby
+# deletes all the characters in the param from the string object
+puts
+# doesnt modify original string
+str = "I am a string"
+puts str.delete("s")
+puts str
+# string! deletes
+str.delete!("s")
+puts str
+
+str = "I am a good"
+str.delete!("ao ")
+puts str
+
+puts
+# WAP to define custom delete() method
+str = "I am a string"
+def mydelete(str, param)
+    final_str = ""
+    str.each_char { |ch|
+        if !param.include?(ch)
+            final_str << ch
+        end
+    }
+    final_str
+end
+puts mydelete(str, "as ")
+puts
+def mydelete2(str, param)
+    final_str = ""
+    str.each_char { |ch| final_str << ch unless param.include?(ch) }
+    final_str
+end
+puts mydelete2(str, "as ")
